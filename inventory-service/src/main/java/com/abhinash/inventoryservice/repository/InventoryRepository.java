9e6b6a67526207beb2d7161d12a3fad391079298
Abhinash7643 <abhinash7643@gmail.com>
2022-06-04 15:39:18 +0200

    api changes to return inventory info for multiple products

--- inventory-service/src/main/java/com/abhinash/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/abhinash/inventoryservice/repository/InventoryRepository.java
@@ -3,9 +3,9 @@ package com.abhinash.inventoryservice.repository;
 import com.abhinash.inventoryservice.domain.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
