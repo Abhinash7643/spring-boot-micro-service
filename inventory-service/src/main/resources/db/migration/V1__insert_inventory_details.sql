dcbae1a11129da8b14b4a69069ce70011ebc892a
Abhinash7643 <abhinash7643@gmail.com>
2023-01-14 14:38:01 +0530

    changes database from oracle to mysql

--- inventory-service/src/main/resources/db/migration/V1__insert_inventory_details.sql
+++ inventory-service/src/main/resources/db/migration/V1__insert_inventory_details.sql
@@ -1,2 +1,10 @@
+CREATE TABLE t_inventory
+(
+    id        BIGINT NOT NULL AUTO_INCREMENT,
+    quantity  int,
+    sku_code  varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 INSERT INTO t_inventory(quantity, sku_code) VALUES(100, 'iphone_13');
 INSERT INTO t_inventory(quantity, sku_code) VALUES(0, 'iphone_13_red');
